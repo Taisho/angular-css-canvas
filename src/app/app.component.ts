05b473cee23e6cddd1e32ae9f368699aa43e6ad0
Tichomir Mitkov <MitkovTihomir@gmail.com>
2018-10-10 19:47:38 +0300

    Rotating circle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app works!';
+
+  @ViewChild('canvas') public canvas;
+
+  ngOnInit() {
+  let context = this.canvas.nativeElement.getContext("2d");
+    context.beginPath();
+    context.arc(150, 150, 150, 0, 2*Math.PI);
+
+    context.moveTo(150, 150);
+    context.lineTo(150, 300);
+    context.stroke();
+  }
 }
